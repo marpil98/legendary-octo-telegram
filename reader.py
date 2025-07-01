19d91c43cd217b601576e66191a2edb36d7a59b8
Marcin <mar.pilewski98@gmail.com>
2025-07-01 19:02:04 +0200

    Adding reader, summarizer, analyzier and translator

---
+++ reader.py
@@ -0,0 +1,72 @@
+import re
+import requests
+from requests.exceptions import MissingSchema
+
+from bs4 import BeautifulSoup as bs
+import pymupdf
+import pymupdf4llm
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+class ArticleReader:
+    
+    def __init__(self, link):
+        
+        self.link=link
+        self._page = None
+        
+        try:
+            
+            try:
+                
+                self._request()
+                
+            except MissingSchema:
+                
+                print("Ładowanie pliku z dokumentu")
+                self.document = pymupdf.open(self.link)
+        
+        except Exception as e:
+            
+            print(e)            
+            print("Nie można odczytać dokumentu. Upewnij się, że przekazany link jest poprawny!")
+            
+    def _request(self):
+        
+        self._page = requests.get(self.link)
+    
+    def read_pdf(self):
+        
+        if self._page is not None:
+            
+            data = self._page.content 
+            self.document = pymupdf.Document(stream=data)
+        
+        else:
+            
+            pass
+        
+        try:
+            
+            self.text = pymupdf4llm.to_markdown(self.document)
+            print("Załadowano plik do markdown")
+            self._markdown = 1
+            
+        except:
+                
+            self._connect_text()
+            print("Załadowano zwykły tekst")
+            self._markdown = 0
+            
+    def _connect_text(self):
+        
+        text = ''
+        
+        for page in self.document:
+            
+            print(page.get_text())
+            text += page.get_text()
+            
+        self.text = text
+        
+
---
+++ summarizer.py
@@ -0,0 +1,67 @@
+class Summarizer:
+    
+    def __init__(self, text, max_chars=1500):
+        
+        self.text = text 
+        self.max_chars = max_chars
+        self._chunk()
+        
+    def _chunk(self):
+        
+        chunks = []
+        current = ''
+        splitted = self.text.split('.')
+        
+        for sentence in splitted:
+            
+            if len(current) + len(sentence):
+                
+                current += sentence + ". "
+                
+            else:
+                
+                chunks.append(current.strip())
+                current = sentence + ". "
+                
+            chunks.append(current.strip())
+        
+        self.chunks = chunks
+        
+    def summarize(self, max_length=500, min_length=200, model="summarize/text_summarization"):        
+        
+        summaries = []
+        
+        for chunk in self.chunks:
+            
+            l = len(chunk.split(' '))
+            min_l = int(.4 * l)
+            max_l = int(.8 * l)
+            summaries.append(self.summ_pipe(chunk, max_length=max_l, min_length=min_l, model=model)[0]['summary_text'])
+            
+        self._sub_summ = summaries
+        all_texts = ' '.join(summaries)
+        self.summary = self.summ_pipe(all_texts, max_length=max_length, min_length=min_length, model=model)
+        
+        return self.summary[0]['summary_text']
+    
+    def summ_pipe(self, chunk, model, max_length=1000, min_length=300):
+        
+        # tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+        # model = AutoModelForSeq2SeqLM.from_pretrained(model)
+        
+        # inputs = tokenizer(
+        #         chunk,
+        #         return_tensors="pt",
+        #         max_length=1024,
+        #         truncation=True
+        #     )
+        # print(inputs["input_ids"])
+        # summary_ids = model.generate(
+        #     inputs["input_ids"],
+        #     max_length=max_length,
+        #     min_length=min_length,
+        #     do_sample=False
+        # )
+        summarizer = pipeline("summarization", model=model,)
+        
+        return summarizer(chunk, max_length=max_length, min_length=min_length, do_sample=True)
